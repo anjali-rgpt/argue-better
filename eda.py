e1087522314c432dc98e7f6c17231bb7c6f52750
qingyang <zhang.qingyang@outlook.com>
2023-03-02 09:25:53 -0800

    update indexer

--- eda.py
+++ eda.py
@@ -1,26 +1,41 @@
 import os
 import sys
+from colbert.infra import Run, RunConfig, ColBERTConfig
+from colbert.data import Queries, Collection
+from colbert import Indexer, Searcher
 
-def read_files(path):
-    file_contents = []
 
-    for filename in os.listdir(path):
-        if filename.endswith('.txt'):
-            with open(os.path.join(path, filename), 'r') as f:
-                content = f.read()
-                file_contents.append(content)
-
-    return file_contents
 
 def main():
-    if len(sys.argv) < 2:
-        print("Please provide the path to the training dataset folder.")
-        return
-    path = sys.argv[1]
-    
-    file_contents = read_files(path)
 
-    print("Number of files read: ", len(file_contents))
+    dataroot = 'data'
+    dataset = 'effective'
+    datasplit = 'train'
+
+    queries = os.path.join(dataroot, dataset, datasplit, 'questions.search.tsv')
+    collection = os.path.join(dataroot, dataset, datasplit, 'collection.tsv')
+
+    queries = Queries(path=queries)
+    collection = Collection(path=collection)
+
+    f'Loaded {len(queries)} queries and {len(collection):,} passages'
+
+    nbits = 2
+    index_name = f'{dataset}.{datasplit}.{nbits}bits'
+
+    with Run().context(RunConfig(nranks=5, experiment='notebook')):
+
+        config = ColBERTConfig(
+            nbits=nbits,
+        )
+        print("initialize indexer")
+        indexer = Indexer(checkpoint='downloads/colbertv2.0', config=config)
+        print("start indexing")
+        indexer.index(name=index_name, collection=collection, overwrite=True)
+
+    with Run().context(RunConfig(experiment='notebook')):
+        print("initialize searcher")
+        searcher = Searcher(index=index_name)
 
 if __name__ == "__main__":
     main()
